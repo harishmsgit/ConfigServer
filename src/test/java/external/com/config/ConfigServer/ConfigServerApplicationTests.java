009ae622c6841e1eb986541b079c286d59c816f2
harishmsgit <senharishms108@gmail.com>
2024-08-11 00:58:00 +0530

    First commit

---
+++ src/test/java/external/com/config/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package external.com.config.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
